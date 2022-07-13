d97dc2ae24401d7d897b4c928cfd3a7d0d25c0e9
Yui1002 <yuimurayama1002@gmail.com>
2022-07-12 18:58:25 -0700

    Complete creating a dynamic Video List component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 // TODO: Render the `App` component to the DOM
-import App from './components/App.js';
+import App from './components/App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
