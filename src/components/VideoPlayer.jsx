f1c26bb0bc7c596ca8145e9c06b38e32de63921b
Yui1002 <yuimurayama1002@gmail.com>
2022-07-13 18:57:17 -0700

    Complete creating a dynamic Video Player component

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,10 +1,12 @@
+
+
 var VideoPlayer = (props) => {
   console.log('VideoPlayer props', props.video);
   // var url = props.video.id.videoId;
   return (
     <div className="video-player">
       <div className="embed-responsive embed-responsive-16by9">
-        <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/${props.video.id.videoId}?autoplay=1`} allowFullScreen></iframe>
+        <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/${props.video.id.videoId}`} allowFullScreen></iframe>
       </div>
       <div className="video-player-details">
         <h3>
